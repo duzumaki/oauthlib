e899fe96cdf2ee96e06da1e37f687ec53c68bb41
David Uzumaki <56260075+duzumaki@users.noreply.github.com>
2024-10-14 21:21:56 +0100

    Allow user code to be configured in device auth flow
    Whilst oauth2 doesn't specify how the format should be
    the current behaviour generates a code that is not human and device friendly.
    
    e.g 6Pp9vPKaanbFydF9omtlNLLdJA4HG7
    
    This commit makes it so that's the default behvaiour but allows the caller of DeviceApplicationServer
    to pass in a user code in a format they prefer in the form of a callable to be called that returns the code as a string

--- oauthlib/oauth2/rfc8628/endpoints/pre_configured.py
+++ oauthlib/oauth2/rfc8628/endpoints/pre_configured.py
@@ -1,21 +1,21 @@
+from oauthlib.oauth2 import TokenEndpoint
 from oauthlib.oauth2.rfc8628.endpoints.device_authorization import (
     DeviceAuthorizationEndpoint,
 )
 from typing import Callable
 
-
-class DeviceApplicationServer(DeviceAuthorizationEndpoint):
+class DeviceApplicationServer(DeviceAuthorizationEndpoint, TokenEndpoint):
     """An all-in-one endpoint featuring Authorization code grant and Bearer tokens."""
 
     def __init__(
         self,
         request_validator,
+        interval,
         verification_uri,
         user_code_generator: Callable[[None], str] = None,
         **kwargs,
     ):
         """Construct a new web application server.
-
         :param request_validator: An implementation of
                                   oauthlib.oauth2.rfc8626.RequestValidator.
         :param verification_uri: the verification_uri to be send back.
@@ -25,5 +25,6 @@ class DeviceApplicationServer(DeviceAuthorizationEndpoint):
             self,
             request_validator,
             verification_uri=verification_uri,
+            interval=interval,
             user_code_generator=user_code_generator,
         )
