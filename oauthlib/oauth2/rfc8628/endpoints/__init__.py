0962db8ffd5c9240cc937c290bbdb9288e869857
David Uzumaki <56260075+duzumaki@users.noreply.github.com>
2024-11-04 16:23:01 +0000

    abs imports

--- oauthlib/oauth2/rfc8628/endpoints/__init__.py
+++ oauthlib/oauth2/rfc8628/endpoints/__init__.py
@@ -5,6 +5,5 @@ oauthlib.oauth2.rfc8628
 This module is an implementation of various logic needed
 for consuming and providing OAuth 2.0 Device Authorization RFC8628.
 """
-
-from .device_authorization import DeviceAuthorizationEndpoint
-from .pre_configured import DeviceApplicationServer
+from oauthlib.oauth2.rfc8628.endpoints.device_authorization import DeviceAuthorizationEndpoint
+from oauthlib.oauth2.rfc8628.endpoints.pre_configured import DeviceApplicationServer
